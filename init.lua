924562595afe073f48a3af201fce121b4c239fbc
rtarun9 <rtarun0913@gmail.com>
2024-07-28 07:39:08 +0530

    Added some Mason packages that are required to be installed

--- init.lua
+++ init.lua
@@ -539,6 +539,10 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"lua-language-server",
+				"clangd",
+				"clang-format",
+				"autopep8",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
